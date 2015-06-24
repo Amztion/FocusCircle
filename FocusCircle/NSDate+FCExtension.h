97641f1ad66a36dc3b585a1305bbde96e69b65c2
Liang Zhao <amztion@gmail.com>
2015-06-24 16:29:20 +0800

    为 NSDate 添加获取当前时区当前时间的方法

---
+++ FocusCircle/NSDate+FCExtension.h
@@ -0,0 +1,16 @@
+//
+//  NSDate+FCExtension.h
+//  FocusCircle
+//
+//  Created by Liang Zhao on 15/6/24.
+//  Copyright © 2015年 Liang Zhao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (FCExtension)
+
++(NSDate *)getCurrentTime;
+
+
+@end
---
+++ FocusCircle/NSDate+FCExtension.m
@@ -0,0 +1,22 @@
+//
+//  NSDate+FCExtension.m
+//  FocusCircle
+//
+//  Created by Liang Zhao on 15/6/24.
+//  Copyright © 2015年 Liang Zhao. All rights reserved.
+//
+
+#import "NSDate+FCExtension.h"
+
+@implementation NSDate (FCExtension)
+
++(NSDate*)getCurrentTime{
+    NSDate *nowTimeInGMT = [NSDate date];
+    NSTimeZone *currentTimeZone = [NSTimeZone systemTimeZone];
+    NSInteger seconds = [currentTimeZone secondsFromGMTForDate:nowTimeInGMT];
+    NSDate *localTimeNow = [nowTimeInGMT dateByAddingTimeInterval:seconds];
+    
+    return localTimeNow;
+}
+
+@end
