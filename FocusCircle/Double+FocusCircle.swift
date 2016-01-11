0fd0e72813d1908468b882a29c6909a061db18e4
Amztion <amztion@gmail.com>
2016-01-11 09:10:10 +0800

    重新写了 Doubel 拓展（不知道为什么上一次没追踪上）

---
+++ FocusCircle/Double+FocusCircle.swift
@@ -0,0 +1,21 @@
+//
+//  Double+FocusCircle.swift
+//  FocusCircle
+//
+//  Created by 赵亮 on 16/1/11.
+//  Copyright © 2016年 Liang Zhao. All rights reserved.
+//
+
+import Foundation
+
+extension Double {
+    func convertIntoTimeComponent() -> TimeComponent {
+        var timeComponent = TimeComponent()
+        
+        timeComponent.second = (self % 3600.0) % 60.0
+        timeComponent.minute = ((self % 3600.0) - timeComponent.second ) / 60.0
+        timeComponent.hour = (self - self % 3600) / 3600.0
+        
+        return timeComponent
+    }
+}
